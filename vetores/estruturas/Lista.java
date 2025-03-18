374a6f479b8dd0d71d27b7058edd5002544c8676
JFormigoni <joaoeformigoni14@gmail.com>
2025-03-18 19:15:26 -0300

    Add files via upload

---
+++ vetores/estruturas/Lista.java
@@ -0,0 +1,10 @@
+package estruturas;
+
+public class Lista {
+
+    private No inicio;
+
+    public void Lista() {
+        this.inicio = null;
+    }
+}
---
+++ vetores/estruturas/No.java
@@ -0,0 +1,12 @@
+package estruturas;
+
+public class No {
+    
+    public int dado;
+    public No proximo;
+    
+    public No(int dado) {
+        this.dado = dado;
+        this.proximo = null;
+    }
+}
