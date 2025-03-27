3422b2eefe1e5c25f5c4049782c7715aebee2e2d
jformigoni <joaoeformigoni14@gmail.com>
2025-03-27 19:57:56 -0300

    feat: códigos do professor

--- vetores/VetorApp.java
+++ vetores/VetorApp.java
@@ -1,16 +1,24 @@
-import estruturas.Vetor;
+import estruturas.Lista;
+
 
 public class VetorApp {
-    
-    public static void main(String[] args) {
-        Vetor vetor = new Vetor(10);
-        vetor.adicionar(21);
-        vetor.adicionar(33);
-        vetor.adicionar(19);
-        vetor.adicionar(22);
-        vetor.adicionar(57);
 
-        System.out.println(vetor.getTamanho());
-        vetor.mostrarElementos();
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        
+        lista.adicionarAoInicio(28);
+        lista.adicionarAoInicio(27);
+        lista.adicionarAoInicio(24);
+        lista.adicionarAoInicio(21);
+        lista.adicionarAoInicio(32);
+        lista.adicionarAoInicio(12);
+        lista.adicionarAoInicio(2);
+
+        lista.mostrarNos();
+
+        System.out.println("-----");
+
+        lista.remover(21);
+        lista.mostrarNos();
     }
 }
--- vetores/estruturas/Vetor.java
+++ vetores/estruturas/Vetor.java
@@ -2,55 +2,79 @@ package estruturas;
 
 public class Vetor {
 
-    private Integer[] elementos;
+    public Integer[] elementos; // 3fc -> 4fd
     private int tamanho;
     private int capacidade;
 
     public Vetor(int capacidade) {
-        this.setElementos(new Integer[capacidade]);
-        this.setTamanho(0);
-        this.setCapacidade(capacidade);
+        this.elementos = new Integer[capacidade];
+        this.capacidade = capacidade;
+        this.tamanho = 0;
     }
 
-    public void adicionar(Integer valor){
-        if (this.tamanho == this.capacidade) {
-            // aumentar a capacidade
-            System.out.println("Não é possível adicionar um novo item no vetor");
-            return;
+    public void adicionar(Integer valor) {
+        if (this.capacidade == this.tamanho) {
+            this.aumentarCapacidade();
         }
-        this.elementos[this.getTamanho()] = valor;
+
+        this.elementos[this.tamanho] = valor;
         this.tamanho++;
     }
 
-    public void mostrarElementos(){
-        for (int i = 0; i < this.tamanho; i++){
+    private void aumentarCapacidade() {
+        int novaCapacidade = this.capacidade + (this.capacidade/2);
+        Integer[] novosElementos = new Integer[novaCapacidade];
+
+        for (int i =0; i<this.tamanho; i++) {
+            novosElementos[i] = this.elementos[i];
+        }
+
+        this.elementos = novosElementos;
+        this.capacidade = novaCapacidade;
+    }
+
+    public void mostrarElementos() {
+        System.out.println("-----");
+        System.out.println("Elementos no vetor");
+        for (int i=0; i < tamanho; i++) {
             System.out.println(this.elementos[i]);
         }
+        System.out.println("-----");
     }
 
-    public Integer[] getElementos(){
-        return this.elementos;
+    public void contem(Integer valor) {
+        for (int i=0; i < tamanho; i++) {
+            if (this.elementos[i] == valor) {
+                System.out.println("Valor encontrado");
+                return;
+            }
+        }
+        System.out.println("Valor não encontrado");
     }
 
-    public void setElementos(Integer[] elementos){
-        this.elementos = elementos;
+    public void remover(int posicao) {
+        if (posicao > this.tamanho) {
+            System.out.println("Posição informada é inválida");
+            return;
+        }
+
+        for (int i = posicao; i < this.tamanho-1; i++) {
+            this.elementos[i] = this.elementos[i+1];
+        }
+
+        this.elementos[this.tamanho-1] = null;
+        this.tamanho--;
     }
 
-    public int getTamanho() {
-        return this.tamanho;
-    }
+    public void removerPorValor(Integer valor) {
+        for (int i=0; i<this.tamanho; i++) {
+            if (this.elementos[i] == valor) {
+                this.remover(i);
+                System.out.println("Item removido");
+                return;
+            }
+        }
 
-    public void setTamanho(int tamanho){
-        this.tamanho = tamanho;
+        System.out.println("Item não encontrado");
     }
-
-    public int getCapacidade() {
-        return this.capacidade;
-    }
-
-    public void setCapacidade(int capacidade){
-        this.capacidade = capacidade;
-    }
-
-    public int numeroProcurado = 22;
 }
